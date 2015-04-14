8a1f7d1f6b82fc48c3a37fc445d5c4f8446b779e
Karol Galanciak <karol.galanciak@gmail.com>
2015-04-14 17:24:23 +0200

    raise expressive error when no deleted_ids are returned in response

--- spec/lib/synced/synchronizer_spec.rb
+++ spec/lib/synced/synchronizer_spec.rb
@@ -72,6 +72,28 @@ describe Synced::Synchronizer do
         end
       end
 
+      context "deleted_ids are not present in metadata" do
+        let(:remote_objects) { [remote_object(id: 12, name: "test-12")] }
+        let(:account) { Account.create }
+        let!(:booking) { account.bookings.create(synced_id: 10, name: "test-10") }
+
+        before do
+          allow(account.api).to receive(:paginate).with("bookings",
+            { auto_paginate: true, updated_since: nil }).and_return(remote_objects)
+          expect(account.api).to receive(:last_response)
+            .and_return(double({ meta: {} }))
+        end
+
+        it "raises CannotDeleteDueToNoDeletedIdsError" do
+          expect {
+            Booking.synchronize(scope: account, remove: true)
+          }.to raise_error(Synced::Synchronizer::CannotDeleteDueToNoDeletedIdsError) { |ex|
+            msg = "Cannot delete Bookings. No deleted_ids were returned in API response."
+            expect(ex.message).to eq msg
+          }
+        end
+      end
+
       context "when canceled_at column is present" do
         let(:location) { Location.create(name: "Bahamas") }
         let!(:photo) { Photo.create(synced_id: 12) }
