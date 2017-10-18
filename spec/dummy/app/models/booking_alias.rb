40395e1d2cb951e0162f52e58ea83189f53a2b33
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2017-10-18 16:15:40 +0200

    update

--- spec/dummy/app/models/booking_alias.rb
+++ spec/dummy/app/models/booking_alias.rb
@@ -1,13 +1,9 @@
 class BookingAlias < ActiveRecord::Base
   synced only_updated: true,
-    mapper: -> { EmptyOne },
     endpoint: :bookings
   belongs_to :account
 
   def self.api
     BookingSync::API::Client.new(ENV["ACCESS_TOKEN"])
-  end
-
-  module EmptyOne
   end
 end
